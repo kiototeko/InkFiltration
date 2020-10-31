b157106b0037f6eb65ef90c79add572c5f1f401d
kiototeko <kiototeko@gmail.com>
2020-10-30 21:30:11 -0600

    added payloads

--- receiver/MATLAB/interbits2packets.m
+++ receiver/MATLAB/interbits2packets.m
@@ -1,6 +1,6 @@
-function num_bits = bits2packets(bits, limits, type, parameter, filename)
+function num_bits = interbits2packets(bits, limits, blank, parameter, filename)
 
-if(type == "Blank")
+if(blank)
 
     bitstring = zeros(parameter.szbits-1,1);
     idx2 = 1;
@@ -15,33 +15,35 @@ if(type == "Blank")
     pattern = 'Pt2';
     pt2 = contains(filename, pattern);
     if(pt2)
-        fseek(fID, (parameter.szbits-1)*25, 'bof');
+        fseek(fID, (parameter.szbits-1)*12, 'bof');
     end
 
     while idx2 < length(bits) - parameter.szbits-4
 
         if idx3 <= length(limits) && idx2 > limits(idx3) && idx3 <= 25
-            bitstring = fscanf(fID, '%1i', parameter.szbits-1).';
+            if mod(idx3,2) == pt2
+                bitstring = fscanf(fID, '%1i', parameter.szbits-1).';
+            end
             idx3 = idx3 + 1;
             done = 1;
         end
 
-        if(done == 1 && (bits(idx2) == 1 && bits(idx2+1) == 0 && bits(idx2+2) == 1 && bits(idx2+3) == 0))
+        if(mod(idx3-1,2) == pt2 && done == 1 && (bits(idx2) == 1 && bits(idx2+1) == 0 && bits(idx2+2) == 1 && bits(idx2+3) == 0))
+
 
             parity = mod(sum(bits(idx2+4:idx2+4+parameter.szbits-2)),2);
 
             if previdx3 < idx3
+                
                 num_bits = num_bits + num_bits_tmp;
+                
                 previdx3 = idx3;
                 num_bits_tmp = 0;
             end
-            
             tmp = (parameter.szbits-1) - (parameter.szbits-1)*pdist([bitstring;bits(idx2+4:idx2+4+parameter.szbits-2)], 'hamming');
-            
             if tmp > num_bits_tmp
                 num_bits_tmp = tmp;
             end
-           
             
             if ismember(-1,bits(idx2+4:idx2+4+parameter.szbits-1))
                 idx2 = find(bits(idx2+4:idx2+4+parameter.szbits-1) == -1, 1, 'last')+idx2+4;
@@ -49,18 +51,24 @@ if(type == "Blank")
                 idx2 = idx2 + 1;
             else
                 if isempty(find((bitstring  == bits(idx2+4:idx2+4+parameter.szbits-2)) == 0,1))
+
                     num_packets = num_packets + 1;
                     done = 0;
+
                 end
 
                 idx2 = idx2 +  2;
+
             end
+
         else
             idx2 = idx2 + 1;
         end
     end
 
-elseif(type == "Text")
+   
+
+else
 
     limits = find(bits < 0);
     idx2 = 1;
@@ -77,19 +85,21 @@ elseif(type == "Text")
     pattern = 'Pt2';
     pt2 = contains(filename, pattern);
     if(pt2)
-        fseek(fID, (parameter.szbits-1)*25, 'bof');
+        fseek(fID, (parameter.szbits-1)*13, 'bof');
     end
 
 
     while idx2 < length(bits) - parameter.szbits-4
 
         if idx3 <= length(limits) && idx2 > limits(idx3) && idx3 < 26
-            bitstring = fscanf(fID, '%1i', parameter.szbits-1).';
+            if mod(idx3,2) == ~pt2
+                bitstring = fscanf(fID, '%1i', parameter.szbits-1).';
+            end
             idx3 = idx3 + 1;
             done = 1;
         end
 
-        if(done == 1 && (bits(idx2) == 1 && bits(idx2+1) == 0 && bits(idx2+2) == 1 && bits(idx2+3) == 0))
+        if(mod(idx3-1,2) == ~pt2 && done == 1 && (bits(idx2) == 1 && bits(idx2+1) == 0 && bits(idx2+2) == 1 && bits(idx2+3) == 0))
 
             if ismember(-1,bits(idx2+4:idx2+4+parameter.szbits-1))
                 idx2 = find(bits(idx2+4:idx2+4+parameter.szbits-1) == -1, 1, 'last')+idx2+4;
@@ -99,31 +109,36 @@ elseif(type == "Text")
             parity = mod(sum(bits(idx2+4:idx2+4+parameter.szbits-2)),2);
 
             if previdx3 < idx3
+               
                 num_bits = num_bits + num_bits_tmp;
+               
                 previdx3 = idx3;
                 num_bits_tmp = 0;
             end
-            
             tmp = (parameter.szbits-1) - (parameter.szbits-1)*pdist([bitstring;bits(idx2+4:idx2+4+parameter.szbits-2)], 'hamming');
-            
             if tmp > num_bits_tmp
                 num_bits_tmp = tmp;
-            end
 
+            end
+           
             if parity ~= bits(idx2+4+parameter.szbits-1)
                 idx2 = idx2 + 1;
             else
+
                 if isempty(find((bitstring  == bits(idx2+4:idx2+4+parameter.szbits-2)) == 0,1))
                     num_packets = num_packets + 1;
                     done = 0;
                 end
 
-                idx2 = idx2 +  2;
+                idx2 = idx2 +  2;%4+szbits;
+
             end
+
         else
             idx2 = idx2 + 1;
         end
     end
+    
 end
 
 num_bits = num_bits + num_bits_tmp;
